ec35410f35743e985c420153f127076c6fdb2475
Kierath <kierath_dhugga@hotmail.co.uk>
2025-07-01 14:47:03 +0100

    vercel

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   const [typedText, setTypedText] = useState("");
