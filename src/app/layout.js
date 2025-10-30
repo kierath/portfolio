b0fe7bbba1977c8b1f22a36a1f13ed0a2fa989c0
Kierath <kierath_dhugga@hotmail.co.uk>
2025-10-30 22:12:09 +0000

    fixed more css and layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         {/*pixel font*/}
         <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Playfair+Display:wght@600&display=swap" rel="stylesheet"/>
